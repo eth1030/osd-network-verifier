92a5f73399de26601fa59543419576c441a40429
Emika Hammond <emhammon@redhat.com>
2024-07-17 14:01:15 -0400

    add test framework

--- pkg/verifier/gcp/gcp_verifier_test.go
+++ pkg/verifier/gcp/gcp_verifier_test.go
@@ -5,27 +5,40 @@ import (
 
 	"github.com/openshift/osd-network-verifier/pkg/clients/gcp"
 	"github.com/openshift/osd-network-verifier/pkg/mocks"
+	"github.com/openshift/osd-network-verifier/pkg/probes/curl"
 	gomock "go.uber.org/mock/gomock"
 	computev1 "google.golang.org/api/compute/v1"
 )
 
 func TestGcpVerifier_findUnreachableEndpoints(t *testing.T) {
-	// want to test output with fake serial console outputs
+	/* want to test parsing for findUnreachableEndpoints so want to test multiple fake serial port outputs
+	this means we need to create a fake serial port output and a fake gcp client */
 	mockCtrl := gomock.NewController(t)
 	// call finish at end to assert mock's expectations
 	defer mockCtrl.Finish()
 	// obtain mock gcp client
 	FakeGCPCli := mocks.NewMockGCPClient(mockCtrl)
+	// create fake output
 	out := &computev1.SerialPortOutput{
 		Contents: "fake output",
 	}
+	// expect GetInstancePorts to be called with any arguments and return fake output
 	FakeGCPCli.EXPECT().GetInstancePorts(gomock.Any(), gomock.Any(), gomock.Any()).Return(out, nil)
 
 	cli := GcpVerifier{
-		GcpClient: gcp.Client{
-			&computev1.Service,
-		},
+		GcpClient: gcp.Client{},
 	}
 
-	err := FakeGCPCli.findUnreachableEndpoints("fake project", "fake zone", "fake instance")
+	// need to set client to fake GCP client
+	var clients gcp.GCPClient
+	clients = FakeGCPCli
+	cli.GcpClient = clients
+
+	err := cli.findUnreachableEndpoints("emhammon-test", "us-east-1", "fake instance", curl.Probe{})
+	if err != nil {
+		t.Errorf("findUnreachableEndpoints() got error %v", err)
+	}
+	if !cli.Output.IsSuccessful() {
+		t.Errorf("Success %v", cli.Output)
+	}
 }
